8451418dcad6deb3e4a21337428f96538c41501b
erandadava <erandadava@gmail.com>
2026-01-06 22:07:47 +0700

    chore: add little documentation on test_asset_automation

--- tests/test_asset_automation.py
+++ tests/test_asset_automation.py
@@ -1,6 +1,7 @@
 import pytest
 from playwright.sync_api import Page
 @pytest.mark.only_browser("chromium")
+
 def test_no_broken_images(page: Page):
     page.goto("https://blsky.tech/", timeout=60000, wait_until="domcontentloaded")
     page.wait_for_timeout(3000)
@@ -10,13 +11,13 @@ def test_no_broken_images(page: Page):
     for image in images:
         if image.is_visible() == False:
             continue
-        width = image.evaluate("(element) => element.naturalWidth")
+        width = image.evaluate("(element) => element.naturalWidth") ## Get natural width of image to check if it's loaded
         if width == 0:
             src = image.get_attribute("src")
             broken_images.append(src)
-            print(f"Broken image found: {src}")
+            print(f"Broken image found: {src}") # Log broken image src
 
-    assert len(broken_images) == 0, f"Broken images found: {broken_images}"
+    assert len(broken_images) == 0, f"Broken images found: {broken_images}" # Assert no broken images found, if found = test fail 
 
 def test_css_js_loaded(page: Page):
     failed_resources = []
